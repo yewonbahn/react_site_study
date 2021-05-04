efb388558e5810627a8582408b51af662e3a21ba
yewonbahn <banyaewon98@gmail.com>
2021-05-05 01:25:16 +0900

    first commit

---
+++ src/Components/Router.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {BrowserRouter as Router,Redirect,Route,Switch} from "react-router-dom";
+import Home from "Routes/Home";
+import Search from "Routes/Search";
+import TV from "Routes/TV";
+export default ()=>(
+    <Router>
+       <Switch>
+        <Route path="/" exact component={Home}/>
+        <Route path="/tv" exact component={TV}/>
+        <Route path="/search" exact component={Search}/>
+        <Redirect from="*" to="/" />
+  </Switch>
+    </Router>
+)
