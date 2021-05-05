64978b34c702e22e1d9a7eafe5d1b632f89b8a3f
반예원 <banyaewon98@gmail.com>
2021-05-05 17:29:13 +0900

    third commit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import {Link,withRouter} from "react-router-dom";
 import styled from "styled-components";
 
 const Header = styled.header`
@@ -25,6 +25,9 @@ const Item = styled.li`
   width: 80px;
   height: 50px;
   text-align: center;
+  border-bottom: 5px solid 
+  ${props => props.current ? "#3498db":"transparent"};
+  transition:border-bottom .5s ease-in-out;
 `;
 
 const SLink = styled(Link)`
@@ -33,12 +36,13 @@ const SLink = styled(Link)`
   align-items: center;
   justify-content: center;
 `;
-export default ()=>(
+export default withRouter(({location:{pathname}})=>(
     <Header>
     <List>
-    <Item><SLink to="/">Movies</SLink></Item>
-    <Item><SLink to="/tv">TV</SLink></Item>
-    <Item><SLink to="/search">Search</SLink></Item>
+    <Item current={pathname === "/"}><SLink to="/">Movies</SLink></Item>
+    <Item current={pathname === "/tv"}><SLink to="/tv">TV</SLink></Item>
+    <Item current={pathname === "/search"}><SLink to="/search">Search</SLink></Item>
     
     </List>
-    </Header>);
+    </Header>));
+
